10142cd728d25b5441b800225c49c21433bb15c9
Aahan Rampal <81867704+GoldMight948@users.noreply.github.com>
2025-07-18 02:14:51 +0530

    Update Round_Robin.py
    Updated logic to fFix Round Robin scheduling to correctly handle dynamic process arrivals

--- Round_Robin.py
+++ Round_Robin.py
@@ -1,50 +1,57 @@
-# Input Phase
+#Input Arrival,Burst and Round Robin
 arrival_input = input("Enter Arrival Time (Separated by Spaces): ")  # Input arrival time
 burst_input = input("Enter Burst Time (Separated by Spaces): ")  # Input burst time
 time_quantum = int(input("Enter Time Quantum: "))  # Input time quantum for Round Robin
 
-# Convert inputs to lists
 arrival_time = list(map(int, arrival_input.split()))  # Convert arrival times to list
 burst_time = list(map(int, burst_input.split()))  # Convert burst times to list
 
 # Make a copy of the burst time for remaining burst time tracking
 remaining_time = burst_time[:]
 
-# Number of processes
-num = len(arrival_time)
+num = len(arrival_time) # Number of processes
 
-# Initialize lists
 completion_time = [0] * num  # To store completion time of each process
 waiting_time = [0] * num  # To store waiting time for each process
 turnaround_time = [0] * num  # To store turnaround time for each process
 normalized_tr = [0] * num  # To store normalized turnaround time
 current_time = 0  # Track the current system time
 
-# Process Queue (list of indices of processes in order of arrival)
 process_queue = [i for i in range(num)]  # Start with all processes in arrival order
 
-while process_queue:
-    i = process_queue.pop(0)  # Take the first process in the queue
-    arrival = arrival_time[i]
-    
-    # If the process arrives after the current time, skip forward to when it arrives
-    if current_time < arrival:
-        current_time = arrival
+ready_queue = []
+unarrived = list(range(num))  # All processes initially unarrived
 
-    # Process the current job for a time quantum or less if it can finish
+while ready_queue or unarrived:
+    # Enqueue any process that has now arrived
+    for i in unarrived[:]:  # copy to safely modify unarrived list
+        if arrival_time[i] <= current_time:
+            ready_queue.append(i)
+            unarrived.remove(i)
+
+    if not ready_queue:
+        # No process is ready, advance time to next arrival
+        current_time = min(arrival_time[i] for i in unarrived)
+        continue
+
+    i = ready_queue.pop(0)
     executed_time = min(time_quantum, remaining_time[i])
     remaining_time[i] -= executed_time
     current_time += executed_time
 
-    # If the process has finished executing, calculate completion time
+    # Check if any new process arrived during execution and add it to ready_queue
+    for j in unarrived[:]:
+        if arrival_time[j] <= current_time:
+            ready_queue.append(j)
+            unarrived.remove(j)
+
     if remaining_time[i] == 0:
         completion_time[i] = current_time
-        turnaround_time[i] = completion_time[i] - arrival_time[i]  # TAT = Completion - Arrival
-        waiting_time[i] = turnaround_time[i] - burst_time[i]  # WT = TAT - Burst
-        # No need to add it back to the queue if finished
+        turnaround_time[i] = completion_time[i] - arrival_time[i]
+        waiting_time[i] = turnaround_time[i] - burst_time[i]
     else:
-        # If the process is not finished, add it back to the end of the queue
-        process_queue.append(i)
+        ready_queue.append(i)
+
 
 # Calculate Normalized Turnaround Time and averages
 for i in range(num):
